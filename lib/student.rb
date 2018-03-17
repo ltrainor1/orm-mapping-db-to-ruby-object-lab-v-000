959b97a8934dde7618f16e0159e6ac8bb31d7c65
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-17 16:58:00 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,7 +60,9 @@ class Student
     WHERE grade = 10
     LIMIT 1
     SQL
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new(row)
+    end.first
   end
 
   def all_students_in_grade_X(x)
