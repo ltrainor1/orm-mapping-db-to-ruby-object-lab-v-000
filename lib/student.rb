0353e1640f7a3eb74d94fc27cd5debe4458cdc22
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-17 16:48:06 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,7 +29,7 @@ class Student
     end.first
   end
 
-  def count_all_students_in_grade_9
+  def self.count_all_students_in_grade_9
     sql = <<-SQL
     SELECT * FROM students
     WHERE grade = "9th"
